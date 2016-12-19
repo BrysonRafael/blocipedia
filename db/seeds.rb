1cbc96a7a340e34174c11e92f68f4fca14327b7e
Bryson <bryson.rafael@gmail.com>
2016-12-19 17:21:28 -0600

    Checkpoint 6: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
 require 'random_data'
 
+# Create Users
+25.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
 # Create Wikis
 50.times do
   Wiki.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    title: Faker::Hipster.sentence,
+    body: Faker::Hipster.paragraph
   )
 end
 wikis = Wiki.all
 
 puts "Seed finished"
 puts "#{Wiki.count} wikis created"
+puts "#{User.count} users created"

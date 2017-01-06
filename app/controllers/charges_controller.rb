49477b9400378b0356de87e5a1a2e8df0f28a48b
Bryson <bryson.rafael@gmail.com>
2017-01-06 11:16:58 -0600

    Subscription Established

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,30 +1,45 @@
 class ChargesController < ApplicationController
   include ChargesHelper
   def create
-  # Creates a Stripe Customer object, for associating
-  # with the charge
-  customer = Stripe::Customer.create(
-    email: current_user.email,
-    card: params[:stripeToken]
-  )
+    Stripe.api_key = "sk_test_Xo8AAESZajnBsYkxzYTQcgzl"
 
-  # Where the real magic happens
-  charge = Stripe::Charge.create(
-    customer: customer.id, # Note -- this is NOT the user_id in your app
-    amount: Amount.default,
-    description: "BigMoney Membership - #{current_user.email}",
-    currency: 'usd'
-  )
+    plan = Stripe::Plan.create(
+      :name => "Basic Plan",
+      :id => "basic-monthly",
+      :interval => "month",
+      :currency => "usd",
+      :amount => 0,
+    )
 
-  flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
-  redirect_to "/" # or wherever
+    # Creates a Stripe Customer object, for associating
+    # with the charge
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
 
-  # Stripe will send back CardErrors, with friendly messages
-  # when something goes wrong.
-  # This `rescue block` catches and displays those errors.
-  rescue Stripe::CardError => e
-    flash[:alert] = e.message
-    redirect_to new_charge_path
+    Stripe::Subscription.create(
+      :customer => customer.id,
+        :plan => "basic-monthly",
+    )
+
+    # Where the real magic happens
+    charge = Stripe::Charge.create(
+      customer: customer.id, # Note -- this is NOT the user_id in your app
+      amount: Amount.default,
+      description: "BigMoney Membership - #{current_user.email}",
+      currency: 'usd'
+    )
+
+    flash[:notice] = "Thanks for all the money, #{current_user.email}! Feel free to pay me again."
+    redirect_to "/" # or wherever
+
+    # Stripe will send back CardErrors, with friendly messages
+    # when something goes wrong.
+    # This `rescue block` catches and displays those errors.
+    rescue Stripe::CardError => e
+      flash[:alert] = e.message
+      redirect_to new_charge_path
   end
 
   def new
